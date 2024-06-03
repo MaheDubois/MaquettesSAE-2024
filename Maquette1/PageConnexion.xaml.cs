fc4a67b65554a87d0c0e4cf86aabd5d9815bdac5
MaheDubois <mahe.dubois@etu.univ-smb.fr>
2024-06-03 08:38:39 +0200

    Ajoutez des fichiers projet.

---
+++ Maquette1/PageConnexion.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Maquette1
+{
+    
+
+
+    /// <summary>
+    /// Logique d'interaction pour PageConnexion.xaml
+    /// </summary>
+    public partial class PageConnexion : Window
+    {
+
+        private bool seConnecter = false;
+
+        public PageConnexion()
+        {
+            InitializeComponent();
+        }
+
+        public bool SeConnecter { get => seConnecter; set => seConnecter = value; }
+
+        private void ButtonSeConnecter_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeComponent();
+            SeConnecter = true;
+            
+        }
+    }
+}
---
+++ Maquette1/PageRecapCommande.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Maquette1
+{
+    /// <summary>
+    /// Logique d'interaction pour PageRecapCommande.xaml
+    /// </summary>
+    public partial class PageRecapCommande : Window
+    {
+        private bool seConnecter = false;
+        public PageRecapCommande()
+        {
+            InitializeComponent();
+        }
+        private void ButtonSeConnecter_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeComponent();
+            seConnecter = true;
+
+        }
+
+    }
+}
