e197e0b6e2e6b6509191fd8299dd2c46ff87bffe
bordmatt <bordmatt@D360-S18>
2024-06-04 14:40:27 +0200

    magasins

--- Maquette1/ApplicationData.cs
+++ Maquette1/ApplicationData.cs
@@ -11,36 +11,28 @@ namespace Maquette1
 {
     public class ApplicationData
     {
-
-        //private ObservableCollection<Client> lesClients;
         private NpgsqlConnection connexion = null;   // futur lien à la BD
+        private ObservableCollection<Magasin> lesMagasins;
 
+        public ObservableCollection<Magasin> LesMagasins 
+        { 
+            get { return lesMagasins; }
+            
+            set { lesMagasins = value; }
+        }
 
-        //public ObservableCollection<Client> LesClients
+        //public NpgsqlConnection Connexion
         //{
         //    get
         //    {
-        //        return this.lesClients;
+        //        return this.connexion;
         //    }
 
         //    set
         //    {
-        //        this.lesClients = value;
+        //        this.connexion = value;
         //    }
         //}
-
-        public NpgsqlConnection Connexion
-        {
-            get
-            {
-                return this.connexion;
-            }
-
-            set
-            {
-                this.connexion = value;
-            }
-        }
 
         public ApplicationData()
         {
@@ -64,58 +56,59 @@ namespace Maquette1
                 // juste pour le debug : à transformer en MsgBox
             }
         }
+        
 
 
-            //public void Read()
-            //{
-            //    this.LesClients = new ObservableCollection<Client>();
-            //}
+        //public void Read()
+        //{
+        //    this.LesClients = new ObservableCollection<Client>();
+        //}
 
-            //public int Read()
-            //{
-            //    this.LesClients = new ObservableCollection<Client>();
-            //    String sql = "SELECT id, nom,prenom,email,genre,telephone, dateNaissance FROM Client";
-            //    try
-            //    {
-            //        SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, Connexion);
-            //        DataTable dataTable = new DataTable();
-            //        dataAdapter.Fill(dataTable);
-            //        foreach (DataRow res in dataTable.Rows)
-            //        {
-            //            Client nouveau = new Client(int.Parse(res["id"].ToString()),
-            //            res["nom"].ToString(), res["prenom"].ToString(),
-            //            res["email"].ToString(), DateTime.Parse(res["dateNaissance"].ToString()),
-            //            res["telephone"].ToString(),
-            //            (GenreClient)char.Parse(res["genre"].ToString()));
-            //            LesClients.Add(nouveau);
-            //        }
-            //        return dataTable.Rows.Count;
-            //    }
-            //    catch (SqlException e)
-            //    { Console.WriteLine("pb de requete : " + e); return 0; }
-            //}
-            //public int Create(Client c)
-            //{
-            //    int nb;
-            //    String sql = $"insert into client (nom,prenom,email,genre,telephone, dateNaissance)"
-            //    + $" values ('{c.Nom}','{c.Prenom}','{c.Email}'"
-            //    + $",'{(char)c.Genre}','{c.Telephone}', "
-            //    + $"'{c.DateNaissance.Year}-{c.DateNaissance.Month}-{c.DateNaissance.Day}'); ";
-            //    try
-            //    {
-            //        SqlCommand cmd = new SqlCommand(sql, Connexion);
-            //        nb = cmd.ExecuteNonQuery();
-            //        return nb;
-            //        //nb permet de connaître le nb de lignes affectées par un insert, update, delete
-            //    }
-            //    catch (Exception sqlE)
-            //    {
-            //        Console.WriteLine("pb de requete : " + sql + "" + sqlE);
-            //        // juste pour le debug : à transformer en MsgBox
-            //        return 0;
-            //    }
-            //}
-            public DataTable GetData(string selectSQL)
+        //public int Read()
+        //{
+        //    this.LesClients = new ObservableCollection<Client>();
+        //    String sql = "SELECT id, nom,prenom,email,genre,telephone, dateNaissance FROM Client";
+        //    try
+        //    {
+        //        SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, Connexion);
+        //        DataTable dataTable = new DataTable();
+        //        dataAdapter.Fill(dataTable);
+        //        foreach (DataRow res in dataTable.Rows)
+        //        {
+        //            Client nouveau = new Client(int.Parse(res["id"].ToString()),
+        //            res["nom"].ToString(), res["prenom"].ToString(),
+        //            res["email"].ToString(), DateTime.Parse(res["dateNaissance"].ToString()),
+        //            res["telephone"].ToString(),
+        //            (GenreClient)char.Parse(res["genre"].ToString()));
+        //            LesClients.Add(nouveau);
+        //        }
+        //        return dataTable.Rows.Count;
+        //    }
+        //    catch (SqlException e)
+        //    { Console.WriteLine("pb de requete : " + e); return 0; }
+        //}
+        //public int Create(Client c)
+        //{
+        //    int nb;
+        //    String sql = $"insert into client (nom,prenom,email,genre,telephone, dateNaissance)"
+        //    + $" values ('{c.Nom}','{c.Prenom}','{c.Email}'"
+        //    + $",'{(char)c.Genre}','{c.Telephone}', "
+        //    + $"'{c.DateNaissance.Year}-{c.DateNaissance.Month}-{c.DateNaissance.Day}'); ";
+        //    try
+        //    {
+        //        SqlCommand cmd = new SqlCommand(sql, Connexion);
+        //        nb = cmd.ExecuteNonQuery();
+        //        return nb;
+        //        //nb permet de connaître le nb de lignes affectées par un insert, update, delete
+        //    }
+        //    catch (Exception sqlE)
+        //    {
+        //        Console.WriteLine("pb de requete : " + sql + "" + sqlE);
+        //        // juste pour le debug : à transformer en MsgBox
+        //        return 0;
+        //    }
+        //}
+        public DataTable GetData(string selectSQL)
             {
                 try
                 {
@@ -155,5 +148,28 @@ namespace Maquette1
                 catch (Exception e)
                 { Console.WriteLine("pb à la déconnexion : " + e); }
             }
+        public int Read()
+        {
+            String sql = "SELECT nummagasin,nommagasin,rue,cp,ville,horaire FROM Client";
+            try
+            {
+                NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(sql, Connexion);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                foreach (DataRow res in dataTable.Rows)
+                {
+                    Magasin nouveau = new Magasin(int.Parse(res["nummagasin"].ToString()),
+                    res["nommagasin"].ToString(), res["rue"].ToString(),
+                    res["cp"].ToString(), res["ville"].ToString(),
+                    res["horaire"].ToString());
+                    LesMagasins.Add(nouveau);
+                }
+                return dataTable.Rows.Count;
+            }
+            catch (NpgsqlException e)
+            { Console.WriteLine("pb de requete : " + e); return 0; }
+        }
     }
+
+
 }
---
+++ Maquette1/Magasin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maquette1
+{
+
+    internal class Magasin
+    {
+        private int numMagagasin;
+        private string nomMagasin;
+        private string rue;
+        private string cP;
+        private string ville;
+        private string horaire;
+
+        public int NumMagagasin { get => numMagagasin; set => numMagagasin = value; }
+        public string NomMagasin { get => nomMagasin; set => nomMagasin = value; }
+        public string Rue { get => rue; set => rue = value; }
+        public string CP { get => cP; set => cP = value; }
+        public string Ville { get => ville; set => ville = value; }
+        public string Horaire { get => horaire; set => horaire = value; }
+
+        public Magasin(int numMagagasin, string nomMagasin, string rue, string cP, string ville, string horaire)
+        {
+            this.NumMagagasin = numMagagasin;
+            this.NomMagasin = nomMagasin;
+            this.Rue = rue;
+            this.CP = cP;
+            this.Ville = ville;
+            this.Horaire = horaire;
+        }
+
+
+        
+
+    }
+}
