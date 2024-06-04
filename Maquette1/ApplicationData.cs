3b508a62dee1236a38270de752d62b99089b6330
MaheDubois <mahe.dubois@etu.univ-smb.fr>
2024-06-04 08:15:14 +0200

    data access

--- Maquette1/ApplicationData.cs
+++ Maquette1/ApplicationData.cs
@@ -66,65 +66,94 @@ namespace Maquette1
         }
 
 
-        //public void Read()
-        //{
-        //    this.LesClients = new ObservableCollection<Client>();
-        //}
+            //public void Read()
+            //{
+            //    this.LesClients = new ObservableCollection<Client>();
+            //}
 
-        //public int Read()
-        //{
-        //    this.LesClients = new ObservableCollection<Client>();
-        //    String sql = "SELECT id, nom,prenom,email,genre,telephone, dateNaissance FROM Client";
-        //    try
-        //    {
-        //        SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, Connexion);
-        //        DataTable dataTable = new DataTable();
-        //        dataAdapter.Fill(dataTable);
-        //        foreach (DataRow res in dataTable.Rows)
-        //        {
-        //            Client nouveau = new Client(int.Parse(res["id"].ToString()),
-        //            res["nom"].ToString(), res["prenom"].ToString(),
-        //            res["email"].ToString(), DateTime.Parse(res["dateNaissance"].ToString()),
-        //            res["telephone"].ToString(),
-        //            (GenreClient)char.Parse(res["genre"].ToString()));
-        //            LesClients.Add(nouveau);
-        //        }
-        //        return dataTable.Rows.Count;
-        //    }
-        //    catch (SqlException e)
-        //    { Console.WriteLine("pb de requete : " + e); return 0; }
-        //}
-        //public int Create(Client c)
-        //{
-        //    int nb;
-        //    String sql = $"insert into client (nom,prenom,email,genre,telephone, dateNaissance)"
-        //    + $" values ('{c.Nom}','{c.Prenom}','{c.Email}'"
-        //    + $",'{(char)c.Genre}','{c.Telephone}', "
-        //    + $"'{c.DateNaissance.Year}-{c.DateNaissance.Month}-{c.DateNaissance.Day}'); ";
-        //    try
-        //    {
-        //        SqlCommand cmd = new SqlCommand(sql, Connexion);
-        //        nb = cmd.ExecuteNonQuery();
-        //        return nb;
-        //        //nb permet de connaître le nb de lignes affectées par un insert, update, delete
-        //    }
-        //    catch (Exception sqlE)
-        //    {
-        //        Console.WriteLine("pb de requete : " + sql + "" + sqlE);
-        //        // juste pour le debug : à transformer en MsgBox
-        //        return 0;
-        //    }
-        //}
-        public void DeconnexionBD()
-        {
-            try
+            //public int Read()
+            //{
+            //    this.LesClients = new ObservableCollection<Client>();
+            //    String sql = "SELECT id, nom,prenom,email,genre,telephone, dateNaissance FROM Client";
+            //    try
+            //    {
+            //        SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, Connexion);
+            //        DataTable dataTable = new DataTable();
+            //        dataAdapter.Fill(dataTable);
+            //        foreach (DataRow res in dataTable.Rows)
+            //        {
+            //            Client nouveau = new Client(int.Parse(res["id"].ToString()),
+            //            res["nom"].ToString(), res["prenom"].ToString(),
+            //            res["email"].ToString(), DateTime.Parse(res["dateNaissance"].ToString()),
+            //            res["telephone"].ToString(),
+            //            (GenreClient)char.Parse(res["genre"].ToString()));
+            //            LesClients.Add(nouveau);
+            //        }
+            //        return dataTable.Rows.Count;
+            //    }
+            //    catch (SqlException e)
+            //    { Console.WriteLine("pb de requete : " + e); return 0; }
+            //}
+            //public int Create(Client c)
+            //{
+            //    int nb;
+            //    String sql = $"insert into client (nom,prenom,email,genre,telephone, dateNaissance)"
+            //    + $" values ('{c.Nom}','{c.Prenom}','{c.Email}'"
+            //    + $",'{(char)c.Genre}','{c.Telephone}', "
+            //    + $"'{c.DateNaissance.Year}-{c.DateNaissance.Month}-{c.DateNaissance.Day}'); ";
+            //    try
+            //    {
+            //        SqlCommand cmd = new SqlCommand(sql, Connexion);
+            //        nb = cmd.ExecuteNonQuery();
+            //        return nb;
+            //        //nb permet de connaître le nb de lignes affectées par un insert, update, delete
+            //    }
+            //    catch (Exception sqlE)
+            //    {
+            //        Console.WriteLine("pb de requete : " + sql + "" + sqlE);
+            //        // juste pour le debug : à transformer en MsgBox
+            //        return 0;
+            //    }
+            //}
+            public DataTable GetData(string selectSQL)
             {
-                Connexion.Close();
+                try
+                {
+                    NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(selectSQL, Connexion);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    return dataTable;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("pb avec : " + selectSQL + e.ToString());
+                    return null;
+                }
             }
-            catch (Exception e)
-            { Console.WriteLine("pb à la déconnexion : " + e); }
-        }
+            public int SetData(string setSQL)
+            {
 
+                try
+                {
+                    NpgsqlCommand sqlCommand = new NpgsqlCommand(setSQL, Connexion);
+                    int nbLines = sqlCommand.ExecuteNonQuery();
+                    return nbLines;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("pb avec : " + setSQL + e.ToString());
+                    return 0;
+                }
+            }
 
+            public void DeconnexionBD()
+            {
+                try
+                {
+                    Connexion.Close();
+                }
+                catch (Exception e)
+                { Console.WriteLine("pb à la déconnexion : " + e); }
+            }
     }
 }
